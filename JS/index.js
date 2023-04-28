a077a9e16700cc073d403d455bafb64c60bb4437
tranvy57 <tranvy.art@gmail.com>
2023-04-28 13:51:19 +0700

    add scroll

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,19 @@ let nav = document.querySelector('.nav-elements');
 document.querySelector('.menu').onclick = () =>{
     nav.classList.toggle('active');
 }
+
+
+// nút vị trí
+
+window.addEventListener("scroll", function() {
+    var scrollPositionIndicator = document.querySelector(".scroll-position-indicator");
+    var scrollTop =  document.body.scrollTop||  document.documentElement.scrollTop;
+    var totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrollPercentage = (scrollTop / totalHeight) * 100;
+    console.log(scrollPercentage);
+    scrollPositionIndicator.style.top = scrollPercentage + "%";
+});
+
+document.querySelector(".scroll-position-indicator").addEventListener("click", function() {
+  window.scrollTo(0, 0);
+});
